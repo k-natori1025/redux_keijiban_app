504ced89b1866b0e9fa35391ad637c40f156da75
k-natori1025 <k-natori1025@gmail.com>
2024-03-31 21:24:19 +0900

    投稿機能を追加

---
+++ src/features/Posts.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { PostsData } from "../DummyData";
+
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState: { value: PostsData },
+  reducers: {
+    addPost: (state, action) => {
+      state.value.push(action.payload);
+    }
+  }
+})
+
+export const { addPost } = postSlice.actions;
+export default postSlice.reducer;
